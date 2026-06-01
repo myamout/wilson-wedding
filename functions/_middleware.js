b9bb663d665788ed54841c0d05f065b4a539acf9
Matthew Yamout <matthew.yamout@gmail.com>
2026-06-01 16:55:33 -0700

    add middleware page for routing

---
+++ functions/_middleware.js
@@ -0,0 +1,7 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  if (url.pathname.includes('.')) {
+    return context.next();
+  }
+  return context.env.ASSETS.fetch(new URL('/index.html', url));
+}
